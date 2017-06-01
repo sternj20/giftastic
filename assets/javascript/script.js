edc1a6d7b260219b5c26af2c3cbfa8988591ea0f
sternj20 <jessesternmusic@gmail.com>
2017-05-31 20:41:50 -0700

    added button to add new images

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,9 @@
-/*
-Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. 
-Your app should take the topics in this array and create buttons in your HTML.
-Try using a loop that appends a button for each string in the array.
-When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-	Add an event handler on the buttons
-	Event handler should send an AJAX request to GIPHY API with the title of the button you clicked on 
-	Return 10 static non animated gif images
-	When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-	Under every gif, display its rating (PG, G, so on).
-
-Only once you get images displaying with button presses should you move on to the next step.
-Add a form to your page takes the value from a user input box and adds it into your topics array.
-	form pushes to our array the value
-	triggers a function that iterates through our array and adds buttons for each array
-
-	*/
-
 // dom variables
 
 var buttons = $(".buttons");
 var images = $(".images");
 // array of strings that are topics for giphy images to be generated
-var topics = ['house of cards', 'black mirror', 'silicon valley', 'thirteen reasons why', 'martin'];
+var topics = ['house of cards', 'black mirror', 'silicon valley', 'tabla', 'martin'];
 
 //iterates through our array and adds buttons for each array
 function generateButtons(){
@@ -88,6 +70,17 @@ $(document).ready(function() {
 			//else turn into still 
 			this.src = $(this).attr("data-still");
 		}
+	});
+	//when add new image button is pressed
+	$(document).on('click', '.addImg', function(event){
+		event.preventDefault();
+		var newImg = ($(".newImg"));
+		//push the name of new image to add to topics array
+		topics.push(newImg.val());
+		//clear input value
+		newImg.val('');
+		//call generate buttons function
+		generateButtons();
 	});
 
 });
